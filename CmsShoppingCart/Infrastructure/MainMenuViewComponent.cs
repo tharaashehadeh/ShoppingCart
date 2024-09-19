5ebeb80f1ff4efacbba36ed820dfd90b01a970f8
thara_shehadeh@outlook.com <thara_shehadeh@outlook.com>
2024-09-19 19:24:46 +0300

    shoppingcart

---
+++ CmsShoppingCart/Infrastructure/MainMenuViewComponent.cs
@@ -0,0 +1,31 @@
+﻿using CmsShoppingCart.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CmsShoppingCart.Infrastructure
+{
+    public class MainMenuViewComponent : ViewComponent
+    {
+        private readonly ShoppingCartContext context;
+
+        public MainMenuViewComponent(ShoppingCartContext context)
+        {
+            this.context = context;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var pages = await GetPagesAsync();
+            return View(pages);
+        }
+
+        private Task<List<Page>> GetPagesAsync()
+        {
+            return context.Pages.OrderBy(x => x.Sorting).ToListAsync();
+        }
+    }
+}
